66081d6e718d3293a371c341ec29475528a9fd07
RahimSa <99124709+rahimsa@users.noreply.github.com>
2022-04-09 22:36:22 +0300

    first commit

---
+++ gocorona/script.js
@@ -0,0 +1,24 @@
+
+"use strict"
+
+
+
+/* let num = 1;
+while (num <= 5) {
+	console.log(num);
+	num++;
+} */
+
+/* let num = 1;
+do {
+	console.log(num)
+	num++
+} while (num <= 5); */
+
+
+/* for (let num = 1; num <= 5; num++) {
+	console.log(num)
+} */
+
+
+
